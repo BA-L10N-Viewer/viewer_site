0bdd27cc573cdfb16213b8c4d23ce05c3c063965
sctop <26916108+sctop@users.noreply.github.com>
2024-10-12 00:01:34 +0800

    fix: Scenario DialogueInfo取消<br/>

--- src/components/scenario/DialogueInfo.vue
+++ src/components/scenario/DialogueInfo.vue
@@ -15,12 +15,15 @@ const props = defineProps({
 
 
 <template>
-  <br v-if="dialogueSelectionGroup !== 0 || dialogueSelectionToGroup !== -1"/>
-  <el-tag type="info" v-if="dialogueSelectionGroup !== 0"><span lang="en">SeleGroup: {{dialogueSelectionGroup}}</span></el-tag>
-  <span v-if="dialogueSelectionGroup !== 0 && dialogueSelectionToGroup !== -1">&nbsp;</span>
-  <el-tag type="info" v-if="dialogueSelectionToGroup !== -1"><span lang="en">SeleToGroup: {{dialogueSelectionToGroup}}</span></el-tag>
+  <div class="dialogue-info-tag">
+    <el-tag type="info" v-if="dialogueSelectionGroup !== 0"><span lang="en">SeleGroup: {{dialogueSelectionGroup}}</span></el-tag>
+    <span v-if="dialogueSelectionGroup !== 0 && dialogueSelectionToGroup !== -1">&nbsp;</span>
+    <el-tag type="info" v-if="dialogueSelectionToGroup !== -1"><span lang="en">SeleToGroup: {{dialogueSelectionToGroup}}</span></el-tag>
+  </div>
 </template>
 
 <style scoped>
-
+.dialogue-info-tag {
+  margin-top: 8px;
+}
 </style>
