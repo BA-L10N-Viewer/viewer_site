3f733a603c372cd57eae86bedb77c98f60261f4b
sctop <26916108+sctop@users.noreply.github.com>
2024-11-03 01:20:31 +0800

    BREAKING-CHANGE: PrimeVue主题变更

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import router from './router'
 
 // PrimeVue
 import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import Lara from '@primevue/themes/lara';
 
 // i18n
@@ -21,6 +22,7 @@ import i18n_zh_TW from '@/i18n/zh-TW.json'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { useSetting } from '@/stores/setting'
 import { useSearchVars } from '@/stores/search'
+import { definePreset } from '@primevue/themes'
 
 const app = createApp(App)
 
@@ -42,9 +44,27 @@ app.use(createPinia().use(piniaPluginPersistedstate))
 // app.use(ElementPlus)
 app.use(router)
 app.use(i18n)
+
+const AuraCustomPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#f0f9ff',
+      100: '#e0f2fe',
+      200: '#bae6fd',
+      300: '#7dd3fc',
+      400: '#38bdf8',
+      500: '#0ea5e9',
+      600: '#0284c7',
+      700: '#0369a1',
+      800: '#075985',
+      900: '#0c4a6e',
+      950: '#082f49',
+    }
+  }
+});
 app.use(PrimeVue, {
   theme: {
-    preset: Lara,
+    preset: AuraCustomPreset,
     options: {
       prefix: 'pv',
       darkModeSelector: '.dark-mode-toggler',
