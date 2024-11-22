8897d014448edcecdaeca741c78515f34c0837ab
sctop <26916108+sctop@users.noreply.github.com>
2024-11-22 20:37:15 +0800

    build: 在生产环境中启用source map方便调试

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     vue(),
     vueJsx(),
