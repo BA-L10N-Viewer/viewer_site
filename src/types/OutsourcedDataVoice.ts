5c679d3fba1f3de8c5f0938798d85be8b60d9800
sctop <26916108+sctop@users.noreply.github.com>
2025-05-01 12:28:39 +0800

    fix(typing): 修复人物语音typing，与后端数据匹配

--- src/types/OutsourcedDataVoice.ts
+++ src/types/OutsourcedDataVoice.ts
@@ -2,12 +2,12 @@ import type { NexonL10nData, NexonL10nDataLang } from '@/types/OutsourcedData'
 
 export type NexonCharVoiceNormalTranscription = Record<NexonL10nDataLang, string[][]>
 export type NexonCharVoiceNormalEntry = {
-  Id: string;
-  FilePath: string[];
-  Transcription: NexonCharVoiceNormalTranscription;
+  Id: string
+  FilePath: [string[], string[], string[]]
+  Transcription: NexonCharVoiceNormalTranscription
 }
 export type NexonCharVoiceNormalGroup = {
-  GroupId: string;
+  GroupId: string
   Data: NexonCharVoiceNormalEntry[]
 }
 export type NexonCharVoiceNormal = NexonCharVoiceNormalGroup[]
@@ -15,44 +15,44 @@ export type NexonCharVoiceNormal = NexonCharVoiceNormalGroup[]
 export type NexonCharVoiceEventTranscriptionData = Record<NexonL10nDataLang, string[]>
 export type NexonCharVoiceEventTranscriptionType = 'FirstTime' | 'Rerun' | 'Immortalized'
 export type NexonCharVoiceEventTranscriptionEntry = {
-  Type: NexonCharVoiceEventTranscriptionType;
-  Data: NexonCharVoiceEventTranscriptionData;
+  Type: NexonCharVoiceEventTranscriptionType
+  Data: NexonCharVoiceEventTranscriptionData
 }
 export type NexonCharVoiceEventVoiceEntryParam = {
-  DialogConditionDetail: string;
-  DialogConditionDetailValue: number;
+  DialogConditionDetail: string
+  DialogConditionDetailValue: number
 }
 export type NexonCharVoiceEventVoiceEntry = {
-  Id: string;
-  FilePath: string[];
-  Transcription: NexonCharVoiceEventTranscriptionEntry[];
+  Id: string
+  FilePath: [string[], string[], string[]]
+  Transcription: NexonCharVoiceEventTranscriptionEntry[]
   Param: NexonCharVoiceEventVoiceEntryParam
 }
 export type NexonCharVoiceEventGroupEntry = {
-  GroupId: string;
-  Data: NexonCharVoiceEventVoiceEntry[];
+  GroupId: string
+  Data: NexonCharVoiceEventVoiceEntry[]
 }
 export type NexonCharVoiceEventEntry = {
-  EventId: string;
-  Data: NexonCharVoiceEventGroupEntry[];
+  EventId: string
+  Data: NexonCharVoiceEventGroupEntry[]
 }
 export type NexonCharVoiceEvent = NexonCharVoiceEventEntry[]
 
 export type NexonCharVoiceBattleTranscription = NexonL10nData
 export type NexonCharVoiceBattleEntry = {
-  Id: string;
-  FilePath: string;
+  Id: string
+  FilePath: [string[], string[], string[]]
   Transcription: NexonCharVoiceBattleTranscription
 }
 export type NexonCharVoiceBattleGroup = {
-  GroupId: string;
-  Data: NexonCharVoiceBattleEntry[];
+  GroupId: string
+  Data: NexonCharVoiceBattleEntry[]
 }
 export type NexonCharVoiceBattle = NexonCharVoiceBattleGroup[]
 
 export type NexonCharVoiceEntry = {
-  Normal: NexonCharVoiceNormal;
-  Lobby?: NexonCharVoiceNormal;
-  Battle?: NexonCharVoiceBattle;
-  Event?: NexonCharVoiceEvent;
+  Normal: NexonCharVoiceNormal
+  Lobby?: NexonCharVoiceNormal
+  Battle?: NexonCharVoiceBattle
+  Event?: NexonCharVoiceEvent
 }
